e549f8a5bd3cffcfe290884ed85af8854b325fb5
Sébastien Cevey <seb@cine7.net>
2013-02-18 10:02:52 +0000

    Load Redis

--- Gemfile
+++ Gemfile
@@ -6,8 +6,14 @@ gem 'json'
 #gem 'time-ago-in-words'
 #gem 'auto_excerpt'
 
+gem 'redis'
+
 # to deploy
 #gem 'capistrano'
 
 # to run in prod
 gem 'thin'
+
+group :development do
+  gem 'foreman'
+end
--- app/magazine/server.rb
+++ app/magazine/server.rb
@@ -1,3 +1,14 @@
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 get '/' do
   "hello world"
+end
+
+get '/test-set' do
+  REDIS.set("foo", "bar")
+end
+
+get '/test' do
+  REDIS.get("foo")
 end
